8a4dfece8956e12a50a4458c3998614cb0ae541d
rasinfosec <roy.shoemake@outlook.com>
2021-12-12 19:06:55 -0600

    Create host_calc.py

---
+++ host_calc.py
@@ -0,0 +1,20 @@
+from ipaddress import ip_address
+
+def listHosts(start, end):
+    start = ip_address(start)
+    end = ip_address(end)
+    result = []
+    while start <= end:
+        result.append(str(start))
+        start += 1
+    return result
+
+file = open("ips.txt","r")
+
+for line in file:
+    fields = line.split("-")
+    host1 = fields[0].strip()
+    host2 = fields[1].strip()
+    calcHosts = (listHosts(host1, host2))
+    for i in calcHosts:
+        print (i)
